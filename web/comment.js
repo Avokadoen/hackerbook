5323fe7896d4a9cd3c33efe28a49c251b90fbca4
akerholten <nikolai_aakk@hotmail.com>
2018-09-30 19:46:06 +0200

    added cookielogin in js, and made so you can log in from a topic

--- web/comment.js
+++ web/comment.js
@@ -2,20 +2,25 @@ function postComment(event){
     var answer = "Something went wrong";
     var req = new XMLHttpRequest();
 
-    req.open("POST", "/postcomment", true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.send(JSON.stringify({
-        username: event.username.value, //Fetch username somehow
-        comment: event.commentInput.value
+    if(isLoggedIn()){
+        req.open("POST", window.location.origin + "/postcomment", true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(JSON.stringify({
+            username: loggedInUser, //Fetch username somehow
+            comment: event.commentInput.value
 
-    }));
+        }));
 
 
-    req.onload = function() {
-        answer = this.responseText;
-        document.getElementById("commentMessage").innerHTML = answer;
-        if(answer === "Comment posted"){ //If successful, display message
+        req.onload = function() {
+            answer = this.responseText;
+            document.getElementById("commentMessage").innerHTML = answer;
+            if(answer === "Comment posted"){ //If successful, display message
 
+            }
         }
+    }
+    else {
+        document.getElementById("commentMessage").innerHTML = "Not logged in, please log in to post comment.";
     }
 }
