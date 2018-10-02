d767e08b8967d2fc0f31f808c9acc3eeb0dea1df
akerholten <nikolai_aakk@hotmail.com>
2018-10-02 16:29:43 +0200

    added functionality in javascript to send request to create new topic

---
+++ web/topic.js
@@ -0,0 +1,32 @@
+function postTopic(event){
+    var answer = "Something went wrong";
+    var req = new XMLHttpRequest();
+
+    if(isLoggedIn()){
+        var url = [location.protocol, '//', location.host, location.pathname+"/newtopic"].join('');
+        req.open("POST", url, true);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send(JSON.stringify({
+            username: loggedInUser, //Fetch username somehow
+            title: event.title.value,
+            text: event.topicInput.value
+        }));
+
+
+        req.onload = function() {
+            answer = this.responseText;
+            document.getElementById("postedTopicMessage").innerHTML = answer;
+            if(answer === "Topic posted"){ //If successful, display message
+
+            }
+        }
+    }
+    else {
+        document.getElementById("postedTopicMessage").innerHTML = "Not logged in, please log in to post topic.";
+    }
+}
+
+
+function postTopicVisible(){
+    $("div.postNewTopic").show();
+}
